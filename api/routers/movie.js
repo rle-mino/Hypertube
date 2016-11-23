e17e6b00897408cc7da9d7dd07422df693b89d48
Montasar JARRAYA <mjarraya@e3r2p11.42.fr>
2016-11-23 18:57:41 +0100

    renamed movie/functions -> movie/search

--- api/routers/movie.js
+++ api/routers/movie.js
@@ -1,5 +1,5 @@
 import cron from 'cron';
-import * as functions from '../movie/functions';
+import * as search from '../movie/search';
 import * as scrap from '../movie/scrap';
 
 export default (app) => {
@@ -7,9 +7,9 @@ export default (app) => {
 		res.send('MOVIE ROUTER: OK');
 	});
 	// app.post('/api/movie/moreinfo', functions.getFilmInfo);
-	app.get('/api/movie/fast_search', functions.fastSearch);
-	app.get('/api/movie/top_search', functions.topSearch);
-	app.get('/api/movie/search', functions.search);
+	app.get('/api/movie/fast_search', search.fastSearch);
+	app.get('/api/movie/top_search', search.topSearch);
+	app.get('/api/movie/search', search.search);
 
 	app.get('/api/movie/refreshyts', scrap.yts);
 	app.get('/api/movie/refresheztv', scrap.eztv);
