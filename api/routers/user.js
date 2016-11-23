8756166985ed1d55d1a200110daa8dcbf3ac9b17
Raphael LE MINOR <rle-mino@e3r1p13.42.fr>
2016-11-23 19:56:39 +0100

    remove require from user's router

--- api/routers/user.js
+++ api/routers/user.js
@@ -2,17 +2,19 @@ import passport				from 'passport';
 import expressJwt			from 'express-jwt';
 import * as userController	from '../user/controller';
 import * as cfg				from '../user/jwt/config';
+import passportStrat		from '../user/passport';
+import ctrlGen				from '../user/functions';
 
 export default (app) => {
-	//app.use(passport.initialize());
-	//app.use(expressJwt({
-	//	secret: cfg.jwtSecret,
-	//}).unless({ path: userController.safePath }));
-	//app.use(userController.error);
-	//app.use(userController.checkTokenMid);
+	app.use(passport.initialize());
+	app.use(expressJwt({
+		secret: cfg.jwtSecret,
+	}).unless({ path: userController.safePath }));
+	app.use(userController.error);
+	app.use(userController.checkTokenMid);
 
-	require('../user/passport')(passport);
-	const userFonc = require('../user/functions')(app, passport);
+	passportStrat(passport);
+	const userFonc = ctrlGen(app, passport);
 
 	app.get('/api/user', (req, res) => {
 		res.send('USER ROUTER: OK');
